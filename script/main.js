a687aa218817767fe2e2fba94d096903c251b9fb
rohanmahdik <93382658+RohanMahadik@users.noreply.github.com>
2025-01-20 19:40:32 +0530

    first commit

---
+++ script/main.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.designation', {
+    // Optional parameters
+    slidesPerView: 1,
+    direction: 'vertical',
+    loop: true,
+    // Autoplay configuration
+    autoplay: {
+        delay: 0, // No delay between transitions
+        disableOnInteraction: false, // Keep autoplay active after user interaction
+    },
+
+    speed: 3000, // Speed of the transition (in ms)
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
